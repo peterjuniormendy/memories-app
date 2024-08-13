2ea6cc32316e66502489634ef71a7931fd74a5d5
Peter Junior Mendy <petermendy5@gmail.com>
2024-08-13 03:40:01 +0100

    create a post model

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { getPost } from "../controllers/post.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+router.get("/", getPost);
 
 export default router;
