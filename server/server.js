ed7550fbc35a1bf9954f4374af57d706bdb5dd90
Peter Junior Mendy <petermendy5@gmail.com>
2024-09-02 23:48:18 +0100

    change port

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 // USE ROUTES
 app.use("/posts", postRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const DB_URI = process.env.DB_URL;
 
 // Connect to the DB
